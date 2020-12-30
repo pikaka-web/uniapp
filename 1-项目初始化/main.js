d2c38ad38d0f842bba502f99b65115764b875a3e
皮卡卡 <56927876+pikaka-web@users.noreply.github.com>
2020-12-30 15:03:02 +0800

    项目初始化

---
+++ 1-项目初始化/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App'
+
+Vue.config.productionTip = false
+
+App.mpType = 'app'
+//全局设置占位符
+Vue.prototype.$statusBarHeight = uni.getSystemInfoSync().statusBarHeight;
+
+const app = new Vue({
+    ...App
+})
+app.$mount()
