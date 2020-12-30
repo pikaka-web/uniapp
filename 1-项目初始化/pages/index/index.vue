2827d8c89294fd263c2622423cacbb7aa92cbe85
皮卡卡 <56927876+pikaka-web@users.noreply.github.com>
2020-12-30 15:16:44 +0800

    Add files via upload

---
+++ 1-项目初始化/pages/index/index.vue
@@ -0,0 +1,28 @@
+<template>
+	<view class="content">
+		<view :style="{height:`${statusBarHeight}px`}"></view>
+		sdasdasd
+	</view>
+</template>
+
+<script>
+	//设置占位符
+	//getSystemInfoSync 获取系统信息API
+	//var statusBarHeight = uni.getSystemInfoSync().statusBarHeight;
+	export default {
+		data() {
+			return {
+				statusBarHeight:this.$statusBarHeight
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+</style>
