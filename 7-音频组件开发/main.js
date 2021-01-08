393b5cde4f5d8a7d5cc7d9960a9546609ea4f6aa
皮卡卡 <56927876+pikaka-web@users.noreply.github.com>
2021-01-08 17:34:42 +0800

    Add files via upload

---
+++ 7-音频组件开发/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import store from 'store'
+import App from './App'
+import myIcon from './components/myIcon.vue';
+
+Vue.config.productionTip = false
+
+App.mpType = 'app'
+Vue.prototype.$statusBarHeight = uni.getSystemInfoSync().statusBarHeight;
+//全局挂在一下自定义myIcon
+Vue.component('myIcon',myIcon)
+Vue.prototype.$store = store;
+
+const app = new Vue({
+	store,
+	...App
+})
+app.$mount()
